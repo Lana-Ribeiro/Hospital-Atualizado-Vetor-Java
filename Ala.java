e277af78718d6c31880408b3d9a490a0223e838d
Lana Ribeiro <107940315+Lana-Ribeiro@users.noreply.github.com>
2023-03-02 11:55:49 -0300

    Add files via upload

--- Ala.java
+++ Ala.java
@@ -2,13 +2,15 @@ public class Ala {
 
     private String[] quartos;
     private int ala;
+    public String adicionaQuarto;
 
     public Ala (int capacidadeAla){
         this.quartos = new String[capacidadeAla];
         this.ala =0;
     }
 
-    public void adicionaQuarto(String quartos) throws Exception{
+    public void adicionaQuarto(String quartos) throws Exception{ // método para adicionar o quartos na ala
+        this.aumentaQuartonaAla();
         if(this.ala < this.quartos.length){
             this.quartos[this.ala] = quartos;
             this.ala++;
@@ -23,7 +25,7 @@ public class Ala {
 
     
     @Override
-    public String toString(){
+    public String toString(){  //método toString
         StringBuilder s= new StringBuilder();
         s.append("[");
 
@@ -42,7 +44,7 @@ public class Ala {
 
     }
 
-    public String buscaQuarto(int numquarto) throws Exception{
+    public String buscaQuarto(int numquarto) throws Exception{ //método para fazer a busca dos quartos
         if(numquarto >= 0 && numquarto < ala){
             return quartos[numquarto];
         }   else {
@@ -59,7 +61,8 @@ public class Ala {
         return -1;
     }
 
-    public boolean adicionaQuarto (int numquarto, String quartos) throws Exception{
+    public boolean adicionaQuarto (int numquarto, String quartos) throws Exception{ //Método adicionar quarto buscando o seu numero e ver se eles existe
+        this.aumentaQuartonaAla();
         if(numquarto >=0 && numquarto < ala){
             for(int i = this.ala-1; i>numquarto; i++){
                 this.quartos[i+1] = this.quartos[i];
@@ -73,7 +76,7 @@ public class Ala {
         return true;
     }
 
-    public void aumentaQuartonaAla(){
+    public void aumentaQuartonaAla(){ //Com a Ala nova e reformada dobrou o número de quartos, então fiz um método para receber esses quartos novos aumentando a array antiga
         if(this.ala == this.quartos.length){
             String [] quartosNovos = new String[this.quartos.length * 2];
 
@@ -84,7 +87,7 @@ public class Ala {
         }
     }
 
-    public void removerQuarto(int numquarto) throws Exception{
+    public void removerQuarto(int numquarto) throws Exception{ // Método para mostrar qual quarto esta ocupado e automaticamente indisponivel
 
         if(numquarto >= 0 && numquarto <ala){
             for(int i = numquarto; i<this.ala-1;i++){
@@ -95,19 +98,5 @@ public class Ala {
             throw new Exception("Quarto Inválido!!");
         }
     }
-    
-    
-    
-
-
-
-
-
-
-    
-
-
-
-
-
+ 
 }
--- Hospital.java
+++ Hospital.java
@@ -1,46 +1,56 @@
+// Projeto Hospital LANA RIBEIRO;
+// Manipulação de Arrays;
+// Projeto consiste em uma Ala de hospital que entrou em reforma e aumentou o seu número de Quartos
+// Onde a capacidade inicial era de 10 quartos na Ala mas com a reforma dobrou o número;
+// Ao decorrer do programa tem suas especificações detalhadas sobre suas funções;
+// O usuário pode manipular Consultando quantos quartos tem na ala, disponibilidade, indisponibilidade, busca de quartos e adiconar quartos com a nova reforma;
+
 public class Hospital {
 
-
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-        
-        Ala ala = new Ala(6);
+      
+        Ala ala = new Ala(10); //Construtor (capacidade inicial da ala é de 10 quartos)
 
-        try{
-            ala.adicionaQuarto("Quarto 01");
-            ala.adicionaQuarto("Quarto 02");
-            ala.adicionaQuarto("Quarto 03");
-            ala.adicionaQuarto("Quarto 04");
-            ala.adicionaQuarto("Quarto 05");
-            ala.adicionaQuarto("Quarto 06");
-        } catch(Exception e){
-            e.printStackTrace();
+
+     try{
+           ala.adicionaQuarto("Quarto 01");
+           ala.adicionaQuarto("Quarto 02");
+           ala.adicionaQuarto("Quarto 03");
+           ala.adicionaQuarto("Quarto 04");
+           ala.adicionaQuarto("Quarto 05");
+           ala.adicionaQuarto("Quarto 06");
+           ala.adicionaQuarto("Quarto 07");
+           ala.adicionaQuarto("Quarto 08");
+           ala.adicionaQuarto("Quarto 09");
+           ala.adicionaQuarto("Quarto 10");
+           ala.adicionaQuarto("Quarto 18"); //adicionando quarto 18
+           ala.adicionaQuarto("Quarto 19"); //adicionando quarto 19
+       } catch(Exception e){
+           e.printStackTrace();
         }
 
-        System.out.println("O numero de quartos que tem na Ala A é de: " +ala.ala());
+        System.out.println("\nO numero de quartos que tem na Ala A é de:\n" +ala.ala());
 
-        System.out.print("\nQuartos Novos e Disponiveis na Ala A:  "+ala.toString());
+        System.out.print("\nQuartos Disponiveis na Ala A:\n"+ala.toString());
         
-        try {
-            System.out.println("Quarto 6 removido com Sucesso! " ala.removerQuarto());
+       try {
+        ala.removerQuarto(0); //remover Quarto 1
+    } catch (Exception e) {
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        e.printStackTrace();
+    }
+
+    System.out.println();
+
+    System.out.println("\nQuarto 1 foi ocupado e está Indisponível!\nPortanto os seguintes quartos Diponíveis são:\n" +ala.toString());
 
         try {
-        System.out.println("\nBuscar Quinto Quarto : " +ala.buscaQuarto(4));
+        System.out.println("\nBuscar Quinto Quarto : " +ala.buscaQuarto(3)); // metodo de buscar o quarto 5
         } catch (Exception e){
             e.printStackTrace();
         }
 
-        System.out.println(("Primeiro Quarto: " +ala.quarto1(null)));
-
-
+        
     }
-
-
     
 }
